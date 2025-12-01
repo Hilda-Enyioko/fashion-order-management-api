49fdb72066a5339c52f0f592884bd60fb93d591e
Hilda Enyioko <enyiokohilda@gmail.com>
2025-12-01 20:28:19 +0100

    Add initial customer model

--- fashion_api/customers/models.py
+++ fashion_api/customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Customer(models.Model):
+    customer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
