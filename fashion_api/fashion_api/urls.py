428a3daf779689a477bfdc5ebe1a9cab02b0a49f
Hilda Enyioko <enyiokohilda@gmail.com>
2025-12-02 13:21:48 +0100

    routers created for hyperlinkrelatedfields

--- fashion_api/fashion_api/urls.py
+++ fashion_api/fashion_api/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from customers.views import CustomerViewSet
+from orders.views import OrderViewSet
+
+router = routers.DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
