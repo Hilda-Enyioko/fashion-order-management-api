f899744ca94d807e886dce4482a79b49a7a8e4c0
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2026-01-01 16:14:54 +0100

    Update create_prod_superuser.py

--- create_prod_superuser.py
+++ create_prod_superuser.py
@@ -1,7 +1,6 @@
 import os
 import django
 
-# Set Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashion_api.settings")
 django.setup()
 
@@ -12,12 +11,20 @@ User = get_user_model()
 USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
 EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
 PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+FIRST_NAME = os.environ.get("DJANGO_SUPERUSER_FIRST_NAME", "Admin")
+LAST_NAME = os.environ.get("DJANGO_SUPERUSER_LAST_NAME", "User")
 
 if not PASSWORD:
     raise ValueError("No superuser password set in DJANGO_SUPERUSER_PASSWORD!")
 
 if not User.objects.filter(username=USERNAME).exists():
-    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+    User.objects.create_superuser(
+        username=USERNAME,
+        email=EMAIL,
+        password=PASSWORD,
+        first_name=FIRST_NAME,
+        last_name=LAST_NAME
+    )
     print("Superuser created!")
 else:
     print("Superuser already exists.")
